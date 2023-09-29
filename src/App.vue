3a5ce8dd4b7a240cfbfd4bd53ac059cd627ea5eb
Dan Chui <dchui108@gmail.com>
2023-09-29 21:37:11 +0900

    add questions list

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,144 @@
 <template>
   <div class="ctr">
-    <div class="questions-ctr">
-      <div class="progress">
-        <div class="bar"></div>
-        <div class="status">1 out of 3 questions answered</div>
-      </div>
-      <div class="single-question">
-        <div class="question">Sample Question 1</div>
-        <div class="answers">
-          <div class="answer">Sample Answer 1</div>
-          <div class="answer">Sample Answer 2</div>
-          <div class="answer">Sample Answer 3</div>
-          <div class="answer">Sample Answer 4</div>
-        </div>
-      </div>
-    </div>
-    <div class="result">
-      <div class="title">You got sample result 1!</div>
-      <div class="desc">Enter a short description here about the result.</div>
-    </div>
+    <questions
+      v-if="questionsAnswered < questions.length"
+      :questions="questions"
+    />
+    <result v-else />
     <button type="button" class="reset-btn">Reset</button>
   </div>
 </template>
 
 <script>
+import Questions from "./components/Questions.vue";
+import Result from "./components/Result.vue";
+
 export default {
   name: "App",
+  components: {
+    Questions,
+    Result,
+  },
+  data() {
+    return {
+      questionsAnswered: 0,
+      questions: [
+        {
+          q: "Which of the following statement best define Vue.js?",
+          answers: [
+            {
+              text: "Vue.js is an open-source JavaScript library that is used for developing user interfaces.",
+              is_correct: false,
+            },
+            {
+              text: "Vue.js is an open-source front-end JavaScript framework used for developing user interfaces.",
+              is_correct: true,
+            },
+            {
+              text: "Vue.js is an open-source, cross-platform, JavaScript run-time situation that performs the JavaScript program outside a web browser.",
+              is_correct: false,
+            },
+            {
+              text: "Vue.js is a JavaScript library constructed to facilitate HTML DOM tree traversal and administration, event handling, CSS activity, and Ajax.",
+              is_correct: false,
+            },
+          ],
+        },
+        {
+          q: "Why is Vue.js called a progressive framework?",
+          answers: [
+            {
+              text: "Vue.js is called a progressive framework because it is being changed and developed continually.",
+              is_correct: true,
+            },
+            {
+              text: "Vue.js is called a progressive framework because it facilitates us to create Dynamic User Interfaces and single-page applications.",
+              is_correct: false,
+            },
+            {
+              text: "Vue.js is called a progressive framework because it follows the latest JavaScript standards.",
+              is_correct: false,
+            },
+            {
+              text: "All of the above.",
+              is_correct: false,
+            },
+          ],
+        },
+        {
+          q: "What is the main usage of Vue.js?",
+          answers: [
+            {
+              text: "Vue.js is a dynamic JavaScript framework that is frequently used for developing user interfaces.",
+              is_correct: true,
+            },
+            {
+              text: "Vue.js is a JavaScript library that makes user interfaces for single-page applications by dividing UI into components.",
+              is_correct: false,
+            },
+            {
+              text: "Vue.js uses the MVVM pattern to bind data to certain DOM elements.",
+              is_correct: false,
+            },
+          ],
+        },
+
+        {
+          q: "Which company invented Vue.js?",
+          answers: [
+            {
+              text: "Facebook",
+              is_correct: false,
+            },
+            {
+              text: "Google",
+              is_correct: true,
+            },
+            {
+              text: "Oracle",
+              is_correct: false,
+            },
+          ],
+        },
+
+        {
+          q: "Which of the following is the correct way to install Vue.js in your project?",
+          answers: [
+            {
+              text: "We can install Vue.js by using CDN by including <script> tag in HTML file.",
+              is_correct: false,
+            },
+            {
+              text: "We can install Vue.js by using Node Package Manager (NPM).",
+              is_correct: false,
+            },
+            {
+              text: "You can install Vue.js using Bower.",
+              is_correct: false,
+            },
+            {
+              text: "All of the above.",
+              is_correct: true,
+            },
+          ],
+        },
+      ],
+      results: [
+        {
+          min: 0,
+          max: 2,
+          title: "Try again!",
+          desc: "Do a little more studying and you may succeed!",
+        },
+        {
+          min: 5,
+          max: 5,
+          title: "Wow, you're a genius!",
+          desc: "Studying has definitely paid off for you!",
+        },
+      ],
+    };
+  },
 };
 </script>
 
